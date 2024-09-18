cb4b46fa1807e4169f7a90c58eaf2acf5f3a805c
Prish20 <prish@students.uonbi.ac.ke>
2024-09-18 19:51:57 +0300

    add 401 Unauthorized error handler and /api/v1/unauthorized route

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,12 +13,23 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+# Error handler for 404 Not Found
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+# Error handler for 401 Unauthorized
+
+
+@app.errorhandler(401)
+def unauthorized_error(error) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
